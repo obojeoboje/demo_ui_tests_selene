b0823d45a2561bd003fd11feea86e203e00ec21c
Aleksandr Privalov <obojealexander@gmail.com>
2023-03-28 18:11:49 +0300

    initial commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,14 +4,35 @@ import allure
 import pytest
 from dotenv import load_dotenv
 from selene.support.shared import browser, config
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from utils import attach
 
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_browser():
-    browser.config.base_url = os.getenv("URL")
-    browser.config.hold_browser_open = True
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    driver = webdriver.Remote(
+        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options
+    )
+    browser.config.driver = driver
+    browser.config.base_url = 'https://staya.dog'
     browser.config.window_width = 1920
     browser.config.window_height = 1080
-    # browser.open('')
     yield
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
     browser.quit()

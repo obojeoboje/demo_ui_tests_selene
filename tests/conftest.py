275963a5d83743e54f5eb5d24c3b704ca619e909
Aleksandr Privalov <obojealexander@gmail.com>
2023-03-29 16:28:05 +0300

    initial commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -23,7 +23,7 @@ def setup_browser():
     options.capabilities.update(selenoid_capabilities)
 
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=os.getenv("SELENOID_URL"),
         options=options
     )
     browser.config.driver = driver
